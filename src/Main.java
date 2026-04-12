ff342056256040132910a3d7e5efba3ecd8d8cf5
massielmedina2501-prog <massielmedina2501@alumnos.ubiobio.cl>
2026-04-12 17:11:17 -0400

    Agrego clase Nombre, enum Tratamiento y clase Main

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,35 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner leer = new Scanner(System.in);
 
-        System.out.printf("Hello and welcome!");
+        System.out.println("--- REGISTRO DE NOMBRE ---");
+        System.out.print("Ingrese sus nombres: ");
+        String nom = leer.nextLine();
+        System.out.print("Ingrese su Apellido Paterno: ");
+        String apePat = leer.nextLine();
+        System.out.print("Ingrese su Apellido Materno: ");
+        String apeMat = leer.nextLine();
 
+
+        System.out.println("Seleccione su tratamiento:");
+        System.out.println("1. SR");
+        System.out.println("2. SRA");
+        System.out.print("Opción: ");
+        int opcion = leer.nextInt();
+
+        Tratamiento tratoSeleccionado;
+        if (opcion == 2) {
+            tratoSeleccionado = Tratamiento.SRA;
+        } else {
+            tratoSeleccionado = Tratamiento.SR;
+        }
+
+        Nombre usuario = new Nombre(tratoSeleccionado, nom, apePat, apeMat);
+        System.out.println("\n----------------------------------");
+        System.out.println("Nombre:");
+        System.out.println(usuario.toString());
+        System.out.println("----------------------------------");
     }
 }
