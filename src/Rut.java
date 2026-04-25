ac3b6bd945becc80160df23f1dcfc50e895178bf
denisse.Manzor <denisse.manzor2501@alumnos.ubiobio.cl>
2026-04-25 13:39:46 -0400

    edito clase rut

--- src/Rut.java
+++ src/Rut.java
@@ -1,31 +1,23 @@
 //Denisse Alejandra Manzor Tapia
-
 public class Rut implements IdPersona{
     private int numero;
     private char dv;
 
-    //Constructor
-    public Rut(int numero, char dv) {
-        this.numero = numero;
+    public Rut(int num, char dv) {
+        this.numero = num;
         this.dv = dv;
     }
-
-    //Metodos
     public int getNumero() {
         return numero;
     }
-
     public char getDv() {
         return dv;
     }
 
-    //toString
     @Override
     public String toString() {
-
         String numString = String.valueOf(numero); //convierte el numero a String
         String resultado = ""; //guardara el resultado
-
         int contador = 0;
 
         for (int i = numString.length() - 1; i >= 0; i--) { //parte desde el final
@@ -37,14 +29,11 @@ public class Rut implements IdPersona{
                 contador = 0;
             }
         }
-
         return resultado + "-" + dv;
     }
 
-    //equals
     @Override
     public boolean equals(Object otro) {
-
         if (this == otro) return true;
 
         //verifica si es un objeto rut
@@ -52,24 +41,18 @@ public class Rut implements IdPersona{
 
         //casting
         Rut r = (Rut) otro;
-
         return numero == r.numero && dv == r.dv;
     }
 
     public static Rut of(String rutConDv) {
-
         if (rutConDv == null) return null;
-
         rutConDv = rutConDv.replace(".", ""); //reemplaza los puntos por vacio, osea los elimina
-
         String[] partes = rutConDv.split("-"); // .SEPARA el numero y el dv, el separador es el "-"
 
         if (partes.length != 2) return null; //verifica que se separo en dos
 
         int num = Integer.parseInt(partes[0]); // cambia el numero a int
-
         char dv = partes[1].charAt(0); //cambia el dv a char
-
         return new Rut(num, dv); //crea un nuevo objeto Rut con numero en int y el dv en char (separados)
     }
 }
